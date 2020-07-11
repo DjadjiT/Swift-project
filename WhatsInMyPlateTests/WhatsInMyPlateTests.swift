d469a05ff889ca8ab2cd247906867d2fd1dc8b7d
Djadji <djadji.traore@gmail.com>
2020-07-11 16:57:02 +0100

    Initial Commit

---
+++ WhatsInMyPlateTests/WhatsInMyPlateTests.swift
@@ -0,0 +1,34 @@
+//
+//  WhatsInMyPlateTests.swift
+//  WhatsInMyPlateTests
+//
+//  Created by user172209 on 7/11/20.
+//  Copyright © 2020 Groupe7. All rights reserved.
+//
+
+import XCTest
+@testable import WhatsInMyPlate
+
+class WhatsInMyPlateTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
